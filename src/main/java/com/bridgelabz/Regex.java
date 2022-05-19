9afd322fed5eb414802b1f6e8f29e62fba0a7de2
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-19 11:21:53 +0530

    Added Email

--- src/main/java/com/bridgelabz/Regex.java
+++ src/main/java/com/bridgelabz/Regex.java
@@ -2,12 +2,12 @@ package com.bridgelabz;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Regex
-{
-    public static void main( String[] args ) {
+public class Regex {
+    public static void main(String[] args) {
         Regex regex = new Regex();
         regex.firstNameInput();
         regex.lastNameInput();
+        regex.emailInput();
     }
 
     public void firstNameInput() {
@@ -22,8 +22,7 @@ public class Regex
         }
     }
 
-    public void lastNameInput()
-    {
+    public void lastNameInput() {
 
         String lastNameInput = "Kumar";
         String regex = "^[[a-z]{1}A-Z]{2,}$";
@@ -31,9 +30,21 @@ public class Regex
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(lastNameInput);
         if (matcher.matches()) {
-            System.out.println("\n VALID  lastName " );
+            System.out.println("\n VALID  lastName ");
         } else {
             System.out.println("\n invalid lastName");
+        }
+    }
+
+    public void emailInput() {
+        String emailInput = "abc.xyz@bl.in";
+        String regex = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z]+.[a-z]{2,3}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(emailInput);
+        if (matcher.matches()) {
+            System.out.println("\n VALID  email ID ");
+        } else {
+            System.out.println("\n invalid  email ID");
         }
     }
 }
